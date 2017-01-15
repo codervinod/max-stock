02641ff436fbe9f55a18fe075147a7f3bcaaca3d
Vinod Gupta <vinod.gupta@nutanix.com>
2017-01-15 09:50:06 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+swagger_py_codegen -s max_stock.yaml max-stock -p app
---
+++ api_server/app/v1/routes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+###
+### DO NOT CHANGE THIS FILE
+### 
+### The code is auto generated, your change will be overwritten by 
+### code generating.
+###
+
+from .api.max_gain import MaxGain
+
+
+routes = [
+    dict(resource=MaxGain, urls=['/max_gain'], endpoint='max_gain'),
+]
