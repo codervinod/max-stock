dbfd0bdf5d6a45593cb0e073024ba081cf6926bd
Vinod Gupta <vinod.gupta@nutanix.com>
2017-01-17 14:20:13 -0800

    Added whitenoise for static content.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+from whitenoise import WhiteNoise
 
 import v1
 
 
 def create_app():
-    app = Flask(__name__, static_folder='static')
-    app.register_blueprint(
-        v1.bp,
-        url_prefix='/v1')
-    return app
+  app = Flask(__name__, static_folder='static')
+  app.register_blueprint(
+    v1.bp,
+    url_prefix='/v1')
+  app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
+  return app
+
 
 app = create_app()
 
 if __name__ == '__main__':
-    create_app().run(debug=True, port=9872)
+  create_app().run(debug=True, port=9872)
